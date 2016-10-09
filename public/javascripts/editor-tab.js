92d50cbd318c21f9a4d1595444b9e3ade6c415d0
ty <tianyu.bruce@gmail.com>
2016-10-09 23:13:19 +0800

    集成了konva绘图的demo，可拖拽五角星

--- public/javascripts/editor-tab.js
+++ public/javascripts/editor-tab.js
@@ -13,6 +13,91 @@ function submitCodeToSim() {
 }
 
 $(function() {
+
+	var width = window.innerWidth;
+	var height = window.innerHeight;
+	
+	var tween = null;
+	
+	function addStar(layer, stage) {
+	var scale = Math.random();
+	
+	var star = new Konva.Star({
+	x: Math.random() * stage.getWidth(),
+	y: Math.random() * stage.getHeight(),
+	numPoints: 5,
+	innerRadius: 30,
+	outerRadius: 50,
+	fill: '#89b717',
+	opacity: 0.8,
+	draggable: true,
+	scale: {
+	  x : scale,
+	  y : scale
+	},
+	rotation: Math.random() * 180,
+	shadowColor: 'black',
+	shadowBlur: 10,
+	shadowOffset: {
+	  x : 5,
+	  y : 5
+	},
+	shadowOpacity: 0.6,
+	// custom attribute
+	startScale: scale
+});
+
+layer.add(star);
+}
+var stage = new Konva.Stage({
+	container: 'konvaContainer',
+	width: $("#konvaContainer").width(),
+	height: 500
+});
+
+var layer = new Konva.Layer();
+var dragLayer = new Konva.Layer();
+
+for(var n = 0; n < 30; n++) {
+addStar(layer, stage);
+}
+
+stage.add(layer, dragLayer);
+
+stage.on('dragstart', function(evt) {
+	var shape = evt.target;
+	// moving to another layer will improve dragging performance
+	shape.moveTo(dragLayer);
+	stage.draw();
+	
+	if (tween) {
+	  tween.pause();
+	}
+	shape.setAttrs({
+	  shadowOffset: {
+	    x: 15,
+	    y: 15
+	  },
+	  scale: {
+	    x: shape.getAttr('startScale') * 1.2,
+	    y: shape.getAttr('startScale') * 1.2
+	  }
+	});
+});
+
+stage.on('dragend', function(evt) {
+	var shape = evt.target;
+	shape.moveTo(layer);
+	stage.draw();
+	shape.to({
+		duration: 0.5,
+		easing: Konva.Easings.ElasticEaseOut,
+		scaleX: shape.getAttr('startScale'),
+		scaleY: shape.getAttr('startScale'),
+		shadowOffsetX: 5,
+		shadowOffsetY: 5
+	});
+});
     // 提交代码
     $('#codeSubmit').click(function() {
         submitCodeToSim();
@@ -24,7 +109,7 @@ $(function() {
         var previousTag = $(e.relatedTarget).attr("href");
         if (currentTag === "#vhdl" && previousTag === "#visual") {
             // 将可视化结构转换为VHDL代码
-            // 
+            //
             $('#vhdlCode').text("LIBRARY IEEE;\nUSE IEEE.STD_LOGIC_1164.ALL;\nUSE IEEE.STD_LOGIC_ARITH.ALL;\nUSE IEEE.STD_LOGIC_UNSIGNED.ALL;\n\nentity digitalEO is\n    port(\n        oe:in STD_LOGIC;\n        six6:out STD_LOGIC;\n        six7:out STD_LOGIC;\n        egt8:out STD_LOGIC;\n        egt9:out STD_LOGIC;\n        eiA:out STD_LOGIC;\n        eiB:out STD_LOGIC\n        );\nend entity;\n\narchitecture eo_digital of digitalEO is\nbegin\n    six6 <= not oe;\n    egt8 <= not oe;\n    eiA <= not oe;\n    six7 <= oe;\n    egt9 <= oe;\n    eiB <= oe;\nend eo_digital;")
         }
     });
