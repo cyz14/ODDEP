1915f595feceda5a533db195e04d07eeb68f8c87
cmh <cmh14@mails.tsinghua.edu.cn>
2016-10-12 06:10:19 -0700

    modelsim local

---
+++ sim/child.js
@@ -0,0 +1,20 @@
+var sys = require('sys')
+var exec = require('child_process').exec;
+/*var cmd='cd'+' '+'fielo'+'&&'+
+'vlib'+' '+'work'+' &&'+
+'vcom'+' '+'fileio.vhd'+
+' '+'gen.vhd'+' &&'+
+'vsim'+' '+'-c'+' '+'-do'+' '+'fileio.do'+' '+'fileio';*/
+var cmd='cd sim && vlib work && vcom fileio.vhd gen.vhd && vsim -c -do fileio.do fileio';
+
+
+exports.sim = function() {
+exec(cmd, function (error, stdout, stderr) {
+　sys.print('stdout： ' + stdout);
+　sys.print('stderr： ' + stderr);
+　if (error !== null) {
+　console.log('exec error： ' + error);
+　　}
+　　});
+};
+
