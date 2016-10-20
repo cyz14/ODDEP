ab17fc31aaebeac653e9f6f5e1a0e563ac78e7f9
李成杰 <licj14@mails.tsinghua.edu.cn>
2016-10-20 14:15:07 +0800

    完成了个人信息修改的后台，只做了一点点微小的工作

--- routes/profile.js
+++ routes/profile.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var tp = require('../tiny-promise');
 var dabs = require('../db/dbtop');
+var md5 = require('js-md5');
 
 router.get('/', function(req, res, next) {
     res.render('profile', {title: '设定'});
@@ -10,21 +11,43 @@ router.get('/', function(req, res, next) {
 router.post('/update', function(req, res, next) {
     var user = req.session.user;
     var nickname = req.body.nickname || null;
-    // 需要用tp.bundle来处理多任务并发
-    console.log(req.body);
-    res.send('bad');
-    /*if (nickname) {
-        var db = dabs();
-        tp.promisify.call(db, 'run', 'UPDATE user SET nickname = ? WHERE name = ?',
-            nickname, user)
-        .then(function(err) {
-            if (err) {
-                res.send('bad');
+    var password = req.body.newpasswd || null;
+    var eldpasswd = req.body.passwd || null;
+    var whereStmt = dabs.obj2Stmt('where', { name: user });
+    var setObj = { nickname: nickname, password: password };
+    if (!dabs.isEmptys.call(setObj)) {
+        var db = dabs.db();
+        (function() {
+            if (setObj.password) {
+                return tp.promisify(dabs.md5Salt_auth, user, eldpasswd);
             } else {
-                res.send('ok');
+                return tp.pickle(null, true);
             }
+        }())
+        .then(tp.spread(function(err, row) {
+            if (err) throw err;
+            if (!row) {
+                res.send('auth err');
+                throw 'auth err';
+            }
+            if (setObj.password) {
+                setObj.password = md5(setObj.password + row.salt);
+            }
+            var setStmt = dabs.obj2Stmt('set', setObj);
+            return tp.promisify.call(db, 'run', 'update user ' + setStmt + whereStmt);
+        }))
+        .then(function(err) {
+            if (err) throw err;
+            if (nickname) req.session.nickname = nickname;
+            res.send('ok');
         })
-    }*/
+        .then(dabs.db_close(db), function(err) {
+            db.close();
+            console.log(err);
+            console.log(req.body);
+            res.send('bad');
+        })
+    }
 });
 
 module.exports = router;
