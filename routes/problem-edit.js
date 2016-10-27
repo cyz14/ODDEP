0e1e5483b1ba576b8155a8d9570860b4fc67cb2f
李成杰 <licj14@mails.tsinghua.edu.cn>
2016-10-27 13:24:25 +0800

    题目编辑框架

--- routes/problem-edit.js
+++ routes/problem-edit.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/:pid', function(req, res, next) {
-    //
+    res.render('problem-edit', {title: '编辑' + req.params.pid});
 });
 
 module.exports = router;
