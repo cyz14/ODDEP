21575256e916eaf1808383dedf8ded14e74794e2
李成杰 <licj14@mails.tsinghua.edu.cn>
2016-11-02 23:27:50 +0800

    1000和1001可以正常提交，对于其他题目，并未实现附带激励、自动校验结果的功能，暂时和沙盒一样。

--- public/javascripts/editor-draw-tab.js
+++ public/javascripts/editor-draw-tab.js
@@ -1,23 +1,11 @@
-var hash = window.location.hash;
-var vhdlEditor = ace.edit("vhdlACE");
-
 $(function() {
     $('body').css('min-height', '0px');
     $('#jumpToVHDL').click(function() {
+        var code = toVHDL();
+        console.log(code);
+        $('#vhdlPreview').text(code);
+        $('#modalVHDL').modal({
+            keyboard: true
+        })
     });
-    function hashBody() {
-        var hash = location.hash;
-        console.log(hash);
-        if (hash === '#visual' || hash === '#vhdl') {
-            $('#editorTab a[href="' + hash + '"]').tab('show');
-        }
-        if (hash === '#visual') {
-            $('body').css('overflow', 'scroll');
-            $('nav').focus();
-        } else if (hash === '#vhdl') {
-            $('body').css('overflow', 'auto');
-        }
-    }
-    hashBody();
-    window.onhashchange = hashBody;
 });
