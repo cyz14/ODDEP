b3f2e99ac27dc2a1464c4b33ee7d51dbf17aac89
陈雅正 <cyz14@mails.tsinghua.edu.cn>
2016-11-19 00:36:29 +0800

    加入了离开页面前对用户保存电路的提示

---
+++ public/javascripts/draw2d/ProtectCircuit.js
@@ -0,0 +1,8 @@
+window.onbeforeunload = function() {
+    return "Your unsaved work might lose. Confirm to leave?";
+};
+
+// ToDo: Load saved circuit in localstrorage
+window.onload = function() {
+
+};
